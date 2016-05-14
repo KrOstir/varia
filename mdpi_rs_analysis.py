6e43b840693892ee881a6506ff2d90ce58a79bbf
Krištof Oštir <krostir@gmail.com>
2016-05-14 16:10:47 +0200

    Extract all papers
    MDPI traverse all volumes, issues, papers. Extract volume, issue, paper, url and store to csv. Prewpare for the analysis.

--- mdpi_rs_analysis.py
+++ mdpi_rs_analysis.py
@@ -13,42 +13,52 @@ import requests
 # Remote Sensing URL
 mdpi_url = "http://www.mdpi.com/"
 rs_url = "2072-4292/"
-# Ranges 1 - 8 in 2016
-# TODO automatically find volumes
-# mdpi_url_rs_vols = range(1,9)
-mdpi_url_rs_vols = range(6,7)
 mdpi_file_out = "mdpi_rs_analysis.csv"
+
+print("Analyzing ", mdpi_url+rs_url)
 
 mdpi_f = open(mdpi_file_out, 'w+')
 print("Volume, Issue, Paper, URL", file=mdpi_f)
 
-# TODO Generate list of volumes
-# TODO Generate list of issues
-# TODO Generate list of papers
-for i in mdpi_url_rs_vols:
+mdpi_r = requests.get(mdpi_url+rs_url)
+soup = BeautifulSoup(mdpi_r.text)
+volumes = []
+for link in soup.find_all('a'):
+    link_url = link.get('href')
+    if rs_url in link_url:
+        volumes.append(link_url)
+volumes = volumes[2:] # The first two urls are current issue and past issue
+
+for vol in volumes:
+    b = vol.rfind("/")
+    i = int(vol[b + 1:])
     print("Processing Volume", i)
     vol_url = rs_url+str(i)+"/"
     url_i = mdpi_url+vol_url
     url_ir = requests.get(url_i)
     soup = BeautifulSoup(url_ir.text)
+    issues = []
     for link in soup.find_all('a'):
         link_url = link.get('href')
         if vol_url in link_url:
-            k = link_url.rfind("/")
-            j = int(link_url[k + 1:])
-            print("Volume", i, "Issue", j)
-            iss_url = vol_url+str(j)+"/"
-            url_ij = mdpi_url + iss_url
-            url_ijr = requests.get(url_ij)
-            soup_j = BeautifulSoup(url_ijr.text)
-            for link_j in soup_j.find_all('a'):
-                link_url_j = link_j.get('href')
-                if (iss_url in link_url_j) & ("pdf" in link_url_j):
-                    l = link_url_j.rfind("/pdf")
-                    m = len(iss_url) + 1
-                    p = int(link_url_j[m:l])
-                    url_p = mdpi_url + link_url_j[1:l]
-                    entry = "{0}, {1}, {2}, {3}".format(i, j, p, url_p)
-                    print(entry, file=mdpi_f)
+            issues.append(link_url)
+    issues = sorted(set(issues))
+    for link in issues:
+        k = link.rfind("/")
+        j = int(link[k + 1:])
+        print("Volume", i, "Issue", j)
+        iss_url = vol_url+str(j)+"/"
+        url_ij = mdpi_url + iss_url
+        url_ijr = requests.get(url_ij)
+        soup_j = BeautifulSoup(url_ijr.text)
+        for link_j in soup_j.find_all('a'):
+            link_url_j = link_j.get('href')
+            if (iss_url in link_url_j) & ("pdf" in link_url_j):
+                l = link_url_j.rfind("/pdf")
+                m = len(iss_url) + 1
+                p = int(link_url_j[m:l])
+                url_p = mdpi_url + link_url_j[1:l]
+                entry = "{0}, {1}, {2}, {3}".format(i, j, p, url_p)
+                print(entry, file=mdpi_f)
 # Close file
 mdpi_f.close()
