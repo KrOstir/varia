4766a1b970e1716cf5f586817c358956af223d41
Kristof Ostir <krostir@gmail.com>
2017-01-20 16:00:14 +0100

    BicikeLJ download
    Download and save BicikeLJ data
    Store in formated CSV
    Download meteo data

--- bicikelj_read.py
+++ bicikelj_read.py
@@ -22,8 +22,8 @@ bicikelj_api_key= "0a494317d60d3d556d0755600b078ea6b26af90f"
 # Bycicle, station dynamic infos
 station_data_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Ljubljana&apiKey=" + bicikelj_api_key
 # Outtut file
-station_data_fn = "bicikelj_station_data.csv"
-bicikelj_log = "bicikelj_read.log"
+station_data_fn = "bicikelj_station_data_" + datetime.datetime.now().strftime('%y%m') + ".csv"
+bicikelj_log = "bicikelj_read_" + datetime.datetime.now().strftime('%y%m') + ".log"
 
 # Open log file, all messages are written to log
 log_file = open(bicikelj_log, "a")
@@ -39,10 +39,10 @@ else:
     raise
 station_data_json = json.loads(data)
 station_data = pd.DataFrame(station_data_json).sort_values(["number"]).reset_index(drop=True)
-station_data_real = station_data[['available_bike_stands', 'available_bikes', 'banking', 'bike_stands',
-                                 'bonus', 'last_update', 'number', 'status']].copy()
+station_data_real = station_data[['available_bike_stands', 'available_bikes', 'bike_stands',
+                                 'last_update', 'number']].copy()
 station_data_real["last_update_time"] = pd.to_datetime(station_data_real["last_update"]*1e6)
-station_data_real.drop(["last_update"], 1)
+station_data_real = station_data_real.drop(["last_update"], 1)
 station_data_real = station_data_real.set_index(["last_update_time"])
 # Add to file
 try:
