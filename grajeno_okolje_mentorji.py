7743e67185a9ce7284e8501cbdf27fcb96ab1a8d
Kristof Ostir <krostir@gmail.com>
2021-01-18 19:24:20 +0100

    Add menthors list

--- grajeno_okolje_mentorji.py
+++ grajeno_okolje_mentorji.py
@@ -11,8 +11,7 @@ import pandas as pd
 
 # %%
 # List
-menthors_fn = 'd:/Kristof'
-menthors_fn = 'D:/Kristof/OneDrive - Univerza v Ljubljani/Grajeno okolje/Dokumenti/go_seznam_mentorjev_2122.xlsx'
+menthors_fn = './data/go_seznam_mentorjev_2122.xlsx'
 
 # %%
 # Read
