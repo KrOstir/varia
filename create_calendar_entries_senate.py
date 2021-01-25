aa1739786f0a66f4cfdf0833eaefd9e2f0c8b78d
Kristof Ostir <krostir@gmail.com>
2021-01-25 14:53:49 +0100

    Update creaate calendar senate

--- create_calendar_entries_senate.py
+++ create_calendar_entries_senate.py
@@ -1,54 +1,85 @@
-# coding: utf-8
+# %%
+# Create senate metings calendar
 
-# Create senate meetings calendar
-# 
-# Read and parse TXT, create CSV
-#
-# Krištof Oštir
-# 2017-10-24
-
+# %%
 import pandas as pd
 import io
 from datetime import datetime
 from datetime import timedelta
 
-# File names
-txt_file_fn = "D:/Kristof/Predavanja/urnik/fgg_senat.txt"
-cal_file_fn = "D:/Kristof/Predavanja/urnik/fgg_senat.csv"
 
-# Temporary string
+# %%
+# File names
+txt_file_fn = "./data/fgg_senat.txt"
+cal_file_fn = "./data/fgg_senat.csv"
+
+
+# %%
 buf = io.StringIO()
 # Read and clean file
 txt_file = open(txt_file_fn)
 text = txt_file.read().splitlines()
+txt_file.close()
+text
+
+
+# %%
 for line in text:
     line = line.strip().replace(" ", "")
     if line.endswith('uri'):
+        line = line.split("seja",1)[1]
         line = line.replace("ob", " ").replace("uri", "")
+        print(line)
         buf.write(line + "\n")
-txt_file.close()
 
-# Read CSV to data frame
+
+# %%
 buf.seek(0)
 calendar = pd.read_csv(buf, header=None)
 
-# Parse date
+
+# %%
+calendar
+
+
+# %%
 calendar["Start"] = pd.to_datetime(calendar[0], format = '%d.%m.%Y %H.%M')
 
-# Events take 3 hours
+
+# %%
 calendar["End"] = calendar["Start"] + timedelta(hours=3)
+
+
+# %%
 calendar[0] = "Seja senata"
 calendar.columns.values[0] = "Subject"
+calendar
 
-# Outlook needs start and end date and time
+
+# %%
 calendar['StartDate'] = calendar['Start'].apply(lambda x:x.date())
 calendar['StartTime'] = calendar['Start'].apply(lambda x:x.time())
 calendar['EndDate'] = calendar['End'].apply(lambda x:x.date())
 calendar['EndTime'] = calendar['End'].apply(lambda x:x.time())
+
+
+# %%
+calendar
+
+
+# %%
 del calendar["Start"]
 del calendar["End"]
+calendar
 
-# Write file
+
+# %%
 cal_file = open(cal_file_fn, "w")
 calendar.to_csv(cal_file)
 cal_file.close()
+
+
+# %%
+
+
+
