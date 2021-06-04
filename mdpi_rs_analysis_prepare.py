40700125b7e86097edb77b3a35873c26bde05363
Kristof Ostir <krostir@gmail.com>
2021-06-04 10:54:11 +0200

    Update mdpi_analysis_prepare
    Processing all data
    Skipping if exists

--- mdpi_rs_analysis_prepare.py
+++ mdpi_rs_analysis_prepare.py
@@ -12,6 +12,27 @@
 import urllib.request as ul
 import pandas as pd
 from bs4 import BeautifulSoup as soup
+from tqdm import tqdm
+import os
+
+# %%
+# Remove duplicates from list
+
+
+def remove_duplicates(list):
+    """
+    remove_duplicates Removes duplicates from Python list
+
+    Args:
+        list (list): Incoming list
+
+    Returns:
+        list: List with duplicates
+    """
+    res = []
+    [res.append(x) for x in list if x not in res]
+
+    return res
 
 
 # %%
@@ -20,14 +41,32 @@ from bs4 import BeautifulSoup as soup
 mdpi_url = 'http://www.mdpi.com/'
 rs_issn = '2072-4292'
 rs_url = 'journal/remotesensing'
-mdpi_file_out = '/data/mdpi_rs_analysis.csv'
+# CSV file to store results
+mdpi_file = './data/mdpi_rs_analysis.csv'
+# Skip flag
+skip = True
 
 # %%
 print('Analyzing ISSN:', rs_issn)
 
 # %%
-mdpi_df = pd.DataFrame(columns=['vol_n', 'issue_n', 'articles'])
-mdpi_df
+# Check if output exists and open it
+if skip and os.path.isfile(mdpi_file):
+    mdpi_df = pd.read_csv(mdpi_file)
+    # Delete last record as papers might have updates
+    l_v = mdpi_df['vol_n'].max()
+    l_i = mdpi_df['issue_n'].max()
+    mdpi_df.drop(mdpi_df[
+        (mdpi_df['vol_n'] == l_v) & (mdpi_df['issue_n'] == l_i)].index,
+        inplace=True)
+else:
+    mdpi_df = pd.DataFrame(columns=['vol_n', 'issue_n', 'articles'])
+
+# %%
+mdpi_df.head()
+
+# Papers list
+papers = []
 
 # %%
 # Create list of volumes
@@ -44,81 +83,64 @@ itemlocator = pagesoup.findAll('div', {"class": "journal-browser-volumes"})
 for link in itemlocator[0].findAll('a'):
     volumes.append(link.get('href'))
 
-# %%
-volumes
-# %%
-vol = volumes[1]
+1  # %%
+for vol in volumes:
+    print('Processing {}'.format(vol))
 
-# %%
+    url = mdpi_url + vol
+    req = ul.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    client = ul.urlopen(req)
+    htmldata = client.read()
+    client.close()
+    # Create soup
+    pagesoup = soup(htmldata, "html.parser")
 
-url = mdpi_url + vol
-req = ul.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-client = ul.urlopen(req)
-htmldata = client.read()
-client.close()
-# Create soup
-pagesoup = soup(htmldata, "html.parser")
+    # Issues
+    issues = []
+    itemlocator = pagesoup.findAll('div', {"class": "ul-spaced"})
+    for link in itemlocator[0].findAll('a'):
+        iss = link.get('href')
+        if iss.startswith(vol):
+            issues.append(link.get('href'))
+    issues = remove_duplicates(issues)
 
+    # Process issues
+    for issue in tqdm(issues):
+        # Volume and issue
+        vol_n = int(vol.split('/')[-1])
+        issue_n = int(issue.split('/')[-1])
 
-# %%
-vol
-# %%
-# Issues
-issues = []
-itemlocator = pagesoup.findAll('div', {"class": "ul-spaced"})
-for link in itemlocator[0].findAll('a'):
-    iss = link.get('href')
-    if iss.startswith(vol):
-        issues.append(link.get('href'))
-        print(link.get('href'))
+        # Skip if volume in DF
+        if (vol_n in mdpi_df['vol_n'].values) & (issue_n in mdpi_df['issue_n'].values):
+            continue
 
+        # Get URL
+        url = mdpi_url + issue
+        req = ul.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        client = ul.urlopen(req)
+        htmldata = client.read()
+        client.close()
+        # Create soup
+        pagesoup = soup(htmldata, "html.parser")
 
-# %%
-# Remove duplicates from list
-def remove_duplicates(list):
-    res = []
-    [res.append(x) for x in list if x not in res]
+        # Papers
+        itemlocator = pagesoup.find_all('h1')[0]
+        ils = str(itemlocator).split()
+        articles = int(ils[ils.index('articles') - 1])
 
-    return res
-
-
-# %%
-issues = remove_duplicates(issues)
-issues
-
-# %%
-issue = issues[3]
-issue
-
-# %%
-url = mdpi_url + issue
-req = ul.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-client = ul.urlopen(req)
-htmldata = client.read()
-client.close()
-# Create soup
-pagesoup = soup(htmldata, "html.parser")
-
-# %%
-# Papers
-itemlocator = pagesoup.find_all('h1')[0]
-
-# itemlocator = pagesoup.findAll('div', {"class":"generic-item article-item"})
-
-# %%
-articles = int(str(itemlocator).split()[-3])
-print(articles)
+        # Add papers
+        papers.append([vol_n, issue_n, articles])
 
 
 # %%
-vol_n = int(vol.split('/')[-1])
-issue_n = int(issue.split('/')[-1])
-# %%
-df = pd.DataFrame([[vol_n, issue_n, articles]], columns=[
-                  'vol_n', 'issue_n', 'articles'])
-print(df)
+# Add new data
+df = pd.DataFrame(papers, columns=['vol_n', 'issue_n', 'articles'])
+mdpi_df = mdpi_df.append(df)
 
 # %%
-mdpi_df.append(df)
+# Save to file
+mdpi_df.to_csv(mdpi_file, index=False)
 
+# Finished
+print('Finished')
 # %%
